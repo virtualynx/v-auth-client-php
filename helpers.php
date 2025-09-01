c267b1a49dacfc4bcbc733b11fbad7310aadf76d
virtualynx <eryandi.angga.pratama@gmail.com>
2025-09-01 12:20:16 +0700

    also get userinfo() from token if session not work (mostly used in api routes)

--- helpers.php
+++ helpers.php
@@ -4,6 +4,8 @@
  * Designed to works with laravel
  */
 
+use VLynx\Sso\VAuthSsoClient;
+
 if(!defined('V_AUTH_SESSION_USERINFO')){
     define('V_AUTH_SESSION_USERINFO', 'v_auth_userinfo');
 }
@@ -18,7 +20,19 @@ if (!function_exists('save_userinfo')) {
 
 if (!function_exists('userinfo')) {
     function userinfo() {
-        return session()->get(V_AUTH_SESSION_USERINFO);
+        $userinfo = session()->get(V_AUTH_SESSION_USERINFO);
+
+        if(empty($userinfo)){
+            $server_url = config('app.sso.server_url');
+            $client_id = config('app.sso.client_id');
+            $client_secret = config('app.sso.client_secret');
+            $server_url_local = config('app.sso.server_url_local');
+
+            $sso = new VAuthSsoClient($server_url, $client_id, $client_secret, $server_url_local);
+            $userinfo = $sso->UserInfo();
+        }
+        
+        return $userinfo;
     }
 }else{
     throw new Exception('userinfo exists');
